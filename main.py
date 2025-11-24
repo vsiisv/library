a615767cdb6190684e7129405da9c7b78bf4d184
vsiisv <shores.freezer0u@icloud.com>
2025-11-24 11:52:49 +1000

    Refactor add_book and main. Shorter list

--- main.py
+++ main.py
@@ -7,19 +7,34 @@ def book_list_view(library):
 
 
 def add_book(title, author, year):
-    if title in library:
-        print(f"Книга {title} уже есть в библиотеке. Обновить данные?")
-        if input("Да/нет: ") == "Да".lower():
+    if title.lower() in [t.lower() for t in library]:
+        print(f'Книга "{title}" уже есть в библиотеке. Обновить данные?')
+        if input("Да/нет: ").strip().lower() == "да":
             library[title] = {"author": author, "year": year, "available": None}
-            print(f"Книга {title} обновлена.")
+            print(f'Книга "{title}" обновлена.')
         else:
-            print(f"Книга {title} не обновлена.")
-            return
+            print(f'Книга "{title}" не обновлена.')
 
     else:
         library[title] = {"author": author, "year": year, "available": None}
-        print(f"Книга {title} добавлена в библиотеку.")
-        return
+        print(f'Книга "{title}" добавлена в библиотеку.')
+
+
+def main():
+    book_list_view(library)
+    if input("\nДобавить новую книгу? (Да/нет): ").lower() == "да":
+        title = input("Введите название книги: ")
+        author = input("Введите автора книги: ")
+        while True:
+            try:
+                year = int(input("Введите год выхода книги: "))
+                add_book(title, author, year)
+                break
+            except ValueError:
+                print("Введите год выхода книги в формате целых чисел. Например: 1990")
+
+    else:
+        print("Книга не добавлена.")
 
 
 library = {
@@ -29,82 +44,14 @@ library = {
         "available": True,
     },
     "1984": {"author": "George Orwell", "year": 1949, "available": True},
-    "Mobie Dick": {"author": "Herman Melville", "year": 1851, "available": False},
+    "Moby Dick": {"author": "Herman Melville", "year": 1851, "available": False},
     "Harry Potter and the Philosopher's Stone": {
         "author": "J.K. Rowling",
         "year": 1997,
         "available": True,
     },
-    "Harry Potter and the Chamber of Secrets": {
-        "author": "J.K. Rowling",
-        "year": 1998,
-        "available": True,
-    },
-    "Harry Potter and the Prisoner of Azkaban": {
-        "author": "J.K. Rowling",
-        "year": 1999,
-        "available": False,
-    },
-    "Harry Potter and the Goblet of Fire": {
-        "author": "J.K. Rowling",
-        "year": 2000,
-        "available": True,
-    },
-    "Harry Potter and the Order of the Phoenix": {
-        "author": "J.K. Rowling",
-        "year": 2003,
-        "available": True,
-    },
-    "Harry Potter and the Half-Blood Prince": {
-        "author": "J.K. Rowling",
-        "year": 2005,
-        "available": False,
-    },
-    "Harry Potter and the Deathly Hallows": {
-        "author": "J.K. Rowling",
-        "year": 2007,
-        "available": True,
-    },
-    "V": {"author": "Thomas Pynchon", "year": 1963, "available": True},
-    "Robinson Crusoe": {"author": "Daniel Defoe", "year": 1719, "available": False},
-    "The Holy Flower": {"author": "H. Rider Haggard", "year": 1905, "available": True},
-    "IT": {"author": "Stephen King", "year": 1986, "available": True},
-    "The Mist": {"author": "Stephen King", "year": 1980, "available": False},
-    "The Long Walk": {"author": "Stephen King", "year": 1979, "available": True},
-    "The Game of Thrones": {
-        "author": "George R.R. Martin",
-        "year": 1996,
-        "available": True,
-    },
-    "Dune": {"author": "Frank Herbert", "year": 1965, "available": False},
-    "The Shining": {"author": "Stephen King", "year": 1977, "available": True},
-    "The Running Man": {"author": "Stephen King", "year": 1987, "available": True},
-    "Institute": {"author": "Stephen King", "year": 1991, "available": True},
-    "The Stand": {"author": "Stephen King", "year": 1978, "available": True},
-    "Tom Sawyer": {"author": "Mark Twain", "year": 1876, "available": False},
-    "Mutant 59: The Plastic Eater": {
-        "author": "Kit Pedler",
-        "year": 1971,
-        "available": True,
-    },
     "The Dark Tower": {"author": "Stephen King", "year": 1989, "available": True},
-    "Gekkle Sky": {"author": "Stephen King", "year": 1982, "available": True},
-    "The Dark Half": {"author": "Stephen King", "year": 1995, "available": True},
-    "The Eye of the World": {"author": "Stephen King", "year": 1991, "available": True},
 }
-
-
-def main():
-    book_list_view(library)
-    print()
-    if input("Добавить новую книгу? (Да/нет): ") == "Да".lower():
-        title = input("Введите название книги: ")
-        author = input("Введите автора книги: ")
-        year = input("Введите год выхода книги: ")
-
-        add_book(title, author, year)
-    else:
-        print("Книга не добавлена.")
 
 
 main()
